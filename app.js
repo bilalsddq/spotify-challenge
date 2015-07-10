e35ec75f540d8fd8c95f7486634370b6e20b7d8a
bilalsddq <bilalsddq@hotmail.com>
2015-07-09 23:33:50 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,46 @@
+var data;
+var baseUrl = 'https://api.spotify.com/v1/search?type=track&query='
+var myApp = angular.module('myApp', ['spotify']);
+
+var myCtrl = myApp.controller('myCtrl', function($scope, $http, Spotify) {
+  $scope.audioObject = {}
+  $scope.getSongs = function() {
+    $http.get(baseUrl + $scope.track).success(function(response){
+      data = $scope.tracks = response.tracks.items
+      
+    })
+  }
+  $scope.play = function(song) {
+    if($scope.currentSong == song) {
+      $scope.audioObject.pause()
+      $scope.currentSong = false
+      return
+    }
+    else {
+      if($scope.audioObject.pause != undefined) $scope.audioObject.pause()
+      $scope.audioObject = new Audio(song);
+      $scope.audioObject.play()  
+      $scope.currentSong = song
+    }
+  }
+})
+
+// Add tool tips to anything with a title property
+$('body').tooltip({
+    selector: '[title]'
+});
+
+$(document).ready(function() {
+    $('.form-group').fadeIn(2000);
+});
+
+var data;
+$.ajax({
+  url: 
+  type: "get";
+  success: function(data) {
+    dat = data;
+    Spotify.getAlbum
+  },
+  dataType:"json";
+})
